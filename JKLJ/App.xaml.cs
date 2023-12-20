a97be6bfda23e8d7ffc82f6626a69285de66559c
Kubulax <kubulax25@gmial.com>
2023-12-20 10:24:00 +0100

    Finnished working

--- JKLJ/App.xaml.cs
+++ JKLJ/App.xaml.cs
@@ -18,7 +18,7 @@ namespace JKLJ
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
