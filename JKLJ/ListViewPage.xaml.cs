2a67fb0c19633ff199d6cb6ca9c04da86e690cc3
LukaszJablonski <Lukasz.jablonski44@gmail.com>
2023-12-20 10:35:22 +0100

    fixed naming

--- JKLJ/ListViewPage.xaml.cs
+++ JKLJ/ListViewPage.xaml.cs
@@ -24,7 +24,7 @@ namespace JKLJ
 
         public void Load()
         {
-            string path = App.Dbpath;
+            string path = App.DdPath;
 
             if(File.Exists(path))
             {
