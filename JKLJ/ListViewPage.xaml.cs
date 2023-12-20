12ce353fb2b033da4b0c445cb577301ba99701b0
LukaszJablonski <Lukasz.jablonski44@gmail.com>
2023-12-20 09:51:36 +0100

    Started adding codebehind

--- JKLJ/ListViewPage.xaml.cs
+++ JKLJ/ListViewPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,18 @@ namespace JKLJ
         public ListViewPage()
         {
             InitializeComponent();
+
+            Load();
+        }
+
+        public void Load()
+        {
+            string path = App.DdPath;
+
+            if(File.Exists(path))
+            {
+                string text = File.ReadAllText(path);
+            }
         }
     }
 }
