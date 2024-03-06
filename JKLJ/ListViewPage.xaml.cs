d0ce80b3f99e8187ad85994374e77db672da552c
Łukasz Jabłoński <lukasz.jablonski44@gmail.com>
2024-03-06 15:39:18 +0100

    added_delete_codebehind

--- JKLJ/ListViewPage.xaml.cs
+++ JKLJ/ListViewPage.xaml.cs
@@ -8,12 +8,17 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Newtonsoft.Json.Linq;
+using System.Collections.ObjectModel;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace JKLJ
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListViewPage : ContentPage
     {
+        private ObservableCollection<string> itemList = new ObservableCollection<string>();
+
         public ListViewPage()
         {
             InitializeComponent();
@@ -34,7 +39,18 @@ namespace JKLJ
                 List<BMIResult> results = JsonConvert.DeserializeObject<List<BMIResult>>(text);
 
                 listViewBMI.ItemsSource = results;
+
+                foreach (var item in results)
+                {
+                    itemList.Add(item.ToString());
+                }
+
             }
+
+
+            
+
+            
         }
 
         private void Delete_btn(object sender, EventArgs e)
@@ -45,6 +61,34 @@ namespace JKLJ
             {
                 string text = File.ReadAllText(path);
             }
+
+            string selectedItem = (string)listViewBMI.SelectedItem;
+
+            if (selectedItem == null)
+            {
+                DisplayAlert("Error", "Please select an item to delete.", "OK");
+                return;
+            }
+
+            
+            itemList.Remove(selectedItem);
+
+            
+            JArray jsonArray = new JArray();
+            foreach (var item in itemList)
+            {
+                jsonArray.Add(item);
+            }
+
+            
+            string updatedJson = jsonArray.ToString();
+
+           
+            
+            File.WriteAllText(path, updatedJson);
+
+            DisplayAlert("Success", "Item has been deleted from the JSON file.", "OK");
+
         }
     }
 }
